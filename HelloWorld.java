a6a107fc5bd77a8a2f33d494852c30fc886e67df
mykine <mykine@sina.com>
2019-11-17 17:23:59 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Wolrd~~~");
+		System.out.println("Hello Wolrd  println自动换行~~");
+		System.out.print("Hello World print不自动换行,");
+		System.out.print("hello world,");
+		System.out.print("Hello World 转义符换行 \n");
+		System.out.print("Hello World \n");
+	}
+}
