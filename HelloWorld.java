03ca886aa6070e230085909207e249b53576afc8
mykine <mykine@sina.com>
2019-11-17 18:05:23 +0800

    使用\t制表符来对齐显示的内容

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,5 +8,11 @@ public class HelloWorld
 		System.out.print("hello world,");
 		System.out.print("Hello World 转义符换行 \n");
 		System.out.print("Hello World \n");
+		System.out.println("姓名\t\t年龄\t电话");
+		System.out.println("张三\t\t19岁\t13712332112");
+		System.out.println("李四\t\t20岁\t13712332112");
+		System.out.println("王五王五\t22岁\t13712332113");
+		System.out.println("赵钱孙李123\t19岁\t13712332112");
+		System.out.println("冯陈褚卫蒋沈韩杨19岁\t13712332112");
 	}
 }
