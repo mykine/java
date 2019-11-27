5199e187d959704af1b4b91f85a57bfc6161af87
mykine <mykine@sina.com>
2019-11-27 10:22:59 +0800

    workspace java

---
+++ base/src/arr.java
@@ -0,0 +1,36 @@
+
+public class arr {
+	public static void main(String args[])
+    {
+            /******************** 数组基本用法begin ********************/
+            //1.声明变量
+            int[] score;
+            //2.分配空间
+            score = new int[5];
+            //3.赋值
+            for(int i=0;i<score.length;i++)
+            {
+                    score[i] = i+1;
+            }
+            //4.使用
+            printArr(score);
+            /******************** 数组基本用法end ********************/
+
+
+            /******************** 数组用法2 ********************/
+            int[] score2 = new int[]{1,2,3,4,5};
+            printArr(score2);
+
+            /******************** 数组用法3 ********************/
+            int[] score3 = {1,2,3,4,5};
+            printArr(score3);
+    }
+
+    public static void printArr(int[] arr) {
+            System.out.println("打印数组:");
+            for(int i=0;i<arr.length;i++)
+            {
+                    System.out.println("score["+i+"]="+arr[i]);
+            }
+    }
+}
