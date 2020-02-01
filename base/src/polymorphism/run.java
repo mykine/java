03d60b3d4de3b6e3dd33527793dd01ed9c8c8a9d
mykine <mykine@sina.com>
2020-02-01 13:51:18 +0800

    多态实现的实现方式：父类引用执行子类对象+子类重写了父类方法

--- base/src/polymorphism/run.java
+++ base/src/polymorphism/run.java
@@ -5,7 +5,7 @@ public class run {
 	public static void main(String args[])
 	{
 		Man man = new Man();
-		//多态实现man的feed动作
+		//多态实现的实现方式：父类引用执行子类对象+子类重写了父类方法
 		Dog dog = new Dog("萨摩");
 		man.feed(dog, "骨头");		
 		Cat cat = new Cat("汤姆");
