962e722beda066ca2ba4a66fe780506deae2142e
mykine <mykine@sina.com>
2020-02-02 19:03:41 +0800

    枚举类型规约参数在指定常量范围

--- base/src/polymorphism/Man.java
+++ base/src/polymorphism/Man.java
@@ -1,12 +1,55 @@
 package polymorphism;
 
+import enums.Gender;
+
 /**
  * 饲养员
  * */
 public class Man {
+	
+	private Gender gender;
+	private String name;
+	
+	
+	public Gender getGender() {
+		return gender;
+	}
+
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Man(Gender gender, String name) {		
+		this.gender = gender;
+		this.name = name;
+	}
+	
+	public Man() 
+	{
 		
-	public void feed(Pet pet,String food) {
+	}
+
+	public void feed(Pet pet,String food) 
+	{
 		pet.eat(food);
+	}
+	
+	public void info()
+	{
+		String genderShow = this.gender == Gender.Female ? "女" : "男";
+		System.out.println(this.name+"是个"+genderShow+"饲养员");
 	}
 	
 }
--- base/src/polymorphism/run.java
+++ base/src/polymorphism/run.java
@@ -1,10 +1,13 @@
 package polymorphism;
 
+import enums.Gender;
+
 public class run {
 	
 	public static void main(String args[])
 	{
-		Man man = new Man();
+		Man man = new Man(Gender.Female,"小可爱");
+		man.info();
 		//多态实现的实现方式：父类引用执行子类对象+子类重写了父类方法
 		Dog dog = new Dog("萨摩");
 		man.feed(dog, "骨头");		
