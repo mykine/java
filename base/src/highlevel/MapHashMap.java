6df5e8fdbd06f52bc82a006ab7538c94863e4b0c
mykine <mykine@sina.com>
2020-02-23 19:24:44 +0800

    HashMap类实现map接口

---
+++ base/src/highlevel/MapHashMap.java
@@ -0,0 +1,32 @@
+package highlevel;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import polymorphism.Cat;
+
+/**
+ * 键值对数据结构Map接口
+ * */
+public class MapHashMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap cats = new HashMap();
+		Cat c1 = new Cat("猫1");
+		cats.put("cat1", c1);
+		Cat c2 = new Cat("猫2");
+		cats.put("cat2", c2);
+		Cat c3 = new Cat("猫3");
+		cats.put("cat2", c2);
+		
+		Iterator it = cats.keySet().iterator();
+		while(it.hasNext()) {
+			String key = it.next()+"";
+			Cat cat = (Cat)cats.get(key);
+			System.out.println(key+":"+cat.getName());
+		}
+	}
+
+}
