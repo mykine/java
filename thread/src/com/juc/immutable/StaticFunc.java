bc3dea5d252fffdfbf3779845cc6f9d4697fccbe
mykine <mykine@sina.com>
2021-01-08 20:36:46 +0800

    深入理解构造代码块和静态代码块

--- thread/src/com/juc/immutable/StaticFunc.java
+++ thread/src/com/juc/immutable/StaticFunc.java
@@ -9,9 +9,19 @@ package com.juc.immutable;
      Father构造函数
      子类构造代码块,b=2
      子类构造函数,c=3
+     ------------------------------
+     Father构造代码块,a=1
+     Father构造函数
+     子类构造代码块,b=2
+     子类构造函数,c=9
  * 原因：实例化子类时，子类会现调用隐藏的super方法即父类的构造方法，
  * 静态代码块会在JVM类加载时就执行仅有的一次，
  * 构造代码块在编译阶段被移到构造方法里首行
+ *
+ * * 注意：一个类的静态代码块只会执行一次
+ *        子类的构造函数会执行隐藏的super即父类的构造函数
+ *        无论调用哪个构造函数，构造代码块都会执行，而且相当于构造函数第一行执行
+ *
  * */
 public class StaticFunc extends Father {
 
@@ -24,6 +34,11 @@ public class StaticFunc extends Father {
     }
 
     public StaticFunc() {
+        System.out.println("子类构造函数,c="+c);
+    }
+
+    public StaticFunc(int c) {
+        this.c = c;
         System.out.println("子类构造函数,c="+c);
     }
 
@@ -55,7 +70,14 @@ public class StaticFunc extends Father {
 class FuncTest{
 
     public static void main(String[] args) {
+        /**
+         * 注意：一个类的静态代码块只会执行一次
+         *      子类的构造函数会执行隐藏的super即父类的构造函数
+         *      无论调用哪个构造函数，构造代码块都会执行，而且相当于构造函数第一行执行
+         * */
         StaticFunc staticFunc = new StaticFunc();
+        System.out.println("------------------------------");
+        StaticFunc staticFunc2 = new StaticFunc(9);
     }
 
 }
