5a2ad26342b28bd814f047ebfacd782597609883
mykine <mykine@sina.com>
2021-01-08 18:57:15 +0800

    原子变量操作如同普通变量加锁实现原子性

---
+++ thread/src/com/juc/atomic/AtomicIntegerDemo1.java
@@ -0,0 +1,44 @@
+package com.juc.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 使用原子类AtomicInteger自增
+ * 和
+ * 普通变量自增，多线程下的安全性对比
+ * */
+public class AtomicIntegerDemo1 implements  Runnable{
+
+    public static final AtomicInteger  atomicInteger = new AtomicInteger();
+
+    public static int normalValue = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        AtomicIntegerDemo1 r = new AtomicIntegerDemo1();
+        Thread thread1 = new Thread(r);
+        Thread thread2 = new Thread(r);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println("原子值="+atomicInteger.get());
+        System.out.println("普通值="+normalValue);
+    }
+
+    public void incrementAtomic(){
+        atomicInteger.getAndIncrement();
+    }
+
+    public void incrementNormal(){
+        normalValue++;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            incrementAtomic();
+            incrementNormal();
+        }
+    }
+
+}
